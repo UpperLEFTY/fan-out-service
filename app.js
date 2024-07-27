db554b20436b800df77c44bb158c08e68dded3af
Essence Alexander <upperlefty@zoho.eu>
2024-07-27 16:08:39 -0700

    refactor: Update routes for fanout API
    This commit updates the routes for the fanout API. It renames the existing route from '/api/v1' to '/fanout/v1' and adds a new route '/fanout/v2' for version 2 of the API. This change improves the organization and clarity of the API endpoints.

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ const { logger } = require('./config/logger');
 const app = express();
 const port = 3000;
 
-
 // import routes
-const fanoutRouterV1 = require('./routes/fanout'); // Existing route
-const fanoutRouterV2 = require('./routes/v2/fanout') // New route
+const fanOutRouterV1 = require('./routes/fanout'); // Existing route
+const fanOutRouterV2 = require('./routes/v2/fanout') // New route
 
 app.use(bodyParser.json());
-
-app.use('/api/v1', fanoutRouterV1);
-app.use('/api/v2', fanoutRouterV2);
+app.use('/fanout/v1', fanOutRouterV1);
+app.use('/fanout/v2', fanOutRouterV2);
 
 app.listen(port, () => {
   logger.info(`Server running on port ${port}`);
